6eb9cc588cda98c010980097fef5232dfd06dc89
Luis Jose Nivar <luisjosenivarn@hotmail.com>
2022-09-06 16:26:16 -0400

    Co-authored-by: Brian Uribe <BrianUribe6@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import "./App.css";
+import Tile from "./components/Tile";
 
 function App() {
   return (
     <div className="app">
       {/* TODO Add game components */}
-      <h1>Game placeholder</h1>
+      <h1>Game placeholder</h1>    
     </div>
   );
 }
