2ad4000a998960d5f2e5cdecbd4fde23f86fb3af
Luis Jose Nivar <luisjosenivarn@hotmail.com>
2022-09-11 17:09:51 -0400

    Refactor ScoreBoard  padZero and margin
    Co-authored-by: Brian Uribe <BrianUribe6@users.noreply.github.com>

--- src/components/ScoreBoard/index.tsx
+++ src/components/ScoreBoard/index.tsx
@@ -8,23 +8,22 @@ type ScoreType = {
   bestScore: number;
 };
 
-const padZero = (num: number, length: number) => String(num).padStart();
+function padZero(num: number, length: number) {
+  return String(num).padStart(length, "0");
+}
+
 function ScoreBoard(props: ScoreType) {
-  /**
-   * Add leading Zeros
-   * @see {@link https://stackoverflow.com/questions/1267283/how-can-i-pad-a-value-with-leading-zeros}
-   */
-  const score = ("0000000" + props.score).slice(-7);
-  const bestScore = ("0000000" + props.bestScore).slice(-7);
-  console.log(styles);
+  const score = padZero(props.score, 7);
+  const bestScore = padZero(props.bestScore, 7);
+
   return (
     <div className={`${styles.scoreBoard} ${props.className}`}>
-      <Container className={styles.marginBottom}>
+      <Container className={`${styles.marginBottom} ${styles.padding}`}>
         <p className={styles.title}>Score:</p>
         <p className={styles.score}>{score}</p>
       </Container>
 
-      <Container>
+      <Container className={styles.padding}>
         <p className={styles.title}>Best:</p>
         <p className={styles.score}>{bestScore} </p>
       </Container>
