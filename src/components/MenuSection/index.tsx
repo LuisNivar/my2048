c65628d53a134c920f99a89627a24c0ee1bb98c7
brianuribe6 <brian.uribe@outlook.com>
2022-09-14 00:41:14 -0400

    Remove unnecessary div nesting

--- src/components/MenuSection/index.tsx
+++ src/components/MenuSection/index.tsx
@@ -1,3 +1,4 @@
+import classNames from "classnames";
 import { ReactComponent as MenuIcon } from "../../assets/menu.svg";
 import { ReactComponent as ReloadIcon } from "../../assets/reload.svg";
 import Container from "../Container";
@@ -9,18 +10,13 @@ type menuSection = {
 };
 
 function MenuSection(props: menuSection) {
+  const containerClass = classNames(styles.container, props.className);
   return (
-    <div className={props.className}>
-      <Container className={styles.container}>
-        <h3 className={styles.title}> Elegant 2048 </h3>
-        <IconButton className={styles.menu} Icon={MenuIcon} title="Menu" />
-        <IconButton
-          className={styles.reload}
-          Icon={ReloadIcon}
-          title="Reload"
-        />
-      </Container>
-    </div>
+    <Container className={containerClass}>
+      <h3 className={styles.title}> Elegant 2048 </h3>
+      <IconButton className={styles.menu} Icon={MenuIcon} title="Menu" />
+      <IconButton className={styles.reload} Icon={ReloadIcon} title="Reload" />
+    </Container>
   );
 }
 
