5dacbdc6d7d275ac98c7e53fb249cfff6e3af7ec
brianuribe6 <brian.uribe@outlook.com>
2023-02-19 14:25:29 -0500

    Refactor to load initial game state from Storage

--- src/components/Game/state.ts
+++ src/components/Game/state.ts
@@ -1,13 +1,8 @@
-import { EMPTY_TILE } from "./constants";
+import { EMPTY_TILE, STATE_KEY } from "./constants";
 import { insertRandomTile, isGameOver } from "./utils";
 
-export type BoardSize = {
-  rows: number;
-  cols: number;
-};
-
+//#region Types
 export type GameState = {
-  dimensions: BoardSize;
   tiles: number[][];
   score: number;
   bestScore: number;
@@ -23,27 +18,12 @@ export type Action =
   | {
       type: "reset";
     };
-
-export function createInitialState(initialState: GameState) {
-  const { rows, cols } = initialState.dimensions;
-  const tiles = Array<number[]>(rows);
-  for (let i = 0; i < tiles.length; i++) {
-    tiles[i] = Array<number>(cols).fill(EMPTY_TILE);
-  }
-
-  insertRandomTile(tiles);
-
-  return {
-    ...initialState,
-    tiles,
-  };
-}
+//#endregion
 
 export function GameReducer(state: GameState, action: Action): GameState {
   switch (action.type) {
     case "updated_tiles": {
       const currentScore = state.score + action.score;
-
       return {
         ...state,
         tiles: action.tiles,
@@ -52,13 +32,49 @@ export function GameReducer(state: GameState, action: Action): GameState {
         hasGameEnded: isGameOver(action.tiles),
       };
     }
-
     case "reset": {
-      return createInitialState(state);
+      return newGame(state, state.tiles.length, state.tiles[0].length);
     }
-
-    default:
-      break;
   }
-  return state;
+}
+
+export function createInitialState(
+  initialState: GameState,
+  rows: number,
+  cols: number
+): GameState {
+  const saveGame = loadGameStateFromLocalStorage();
+
+  return saveGame ? saveGame : newGame(initialState, rows, cols);
+}
+
+function loadGameStateFromLocalStorage(): GameState | undefined {
+  const savedState = localStorage.getItem(STATE_KEY);
+
+  if (savedState) {
+    try {
+      return JSON.parse(savedState);
+    } catch (error) {
+      console.error("Error parsing saved state", error);
+    }
+  }
+}
+
+function newGame(state: GameState, rows: number, cols: number): GameState {
+  return {
+    ...state,
+    tiles: createInitialGrid(rows, cols),
+    score: 0,
+    hasGameEnded: false,
+  };
+}
+
+function createInitialGrid(rows: number, cols: number) {
+  const grid = Array<number[]>(rows);
+  for (let i = 0; i < grid.length; i++) {
+    grid[i] = Array<number>(cols).fill(EMPTY_TILE);
+  }
+  insertRandomTile(grid);
+
+  return grid;
 }
